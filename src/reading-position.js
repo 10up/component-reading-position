945fb64cce124836734a83fc24daec2980cdfddf
Fabian Kägy <mail@fabian-kaegy.de>
2020-04-29 20:16:07 +0200

    remove uneccesary $ before elements

--- src/reading-position.js
+++ src/reading-position.js
@@ -32,10 +32,10 @@ export default class ReadingPosition {
 		}
 
 		// ReadingPosition container
-		this.$readingPosition = document.querySelector(elementSelector);
+		this.readingPositionElement = document.querySelector(elementSelector);
 
 		// Bail out if there's no readingPosition.
-		if (!this.$readingPosition) {
+		if (!this.readingPositionElement) {
 			console.error( '10up ReadingPosition: Target not found. A valid target (readingPosition) must be used.'  ); // eslint-disable-line
 			return;
 		}
@@ -46,11 +46,11 @@ export default class ReadingPosition {
 		this.settings = { ...defaults, ...options };
 
 		this.updating = false;
-		this.$startElement = document.querySelector(this.settings.startElement);
-		this.$endElement = document.querySelector(this.settings.endElement);
+		this.startElement = document.querySelector(this.settings.startElement);
+		this.endElement = document.querySelector(this.settings.endElement);
 		this.inScrollArea = false;
 
-		this.setupReadingPosition(this.$readingPosition);
+		this.setupReadingPosition(this.readingPositionElement);
 
 		/**
 		 * Called after the readingPosition is initialized on page load.
@@ -103,7 +103,7 @@ export default class ReadingPosition {
 	 */
 	handleScroll() {
 		// setting the value of the progress bar to the current percentage
-		this.$readingPosition.setAttribute('value', this.percentage);
+		this.readingPositionElement.setAttribute('value', this.percentage);
 
 		// check wether is in scroll area based on the percentage
 		if (this.percentage > 0 && this.percentage < 100) {
@@ -167,9 +167,9 @@ export default class ReadingPosition {
 		let scrollAreaHeight = 0;
 
 		// check wether an endElement is present and a valid DOM Node
-		if (this.$endElement && this.$endElement instanceof HTMLElement) {
+		if (this.endElement && this.endElement instanceof HTMLElement) {
 			// setting the scroll area height to the top ofset of the end element
-			scrollAreaHeight = this.$endElement.offsetTop;
+			scrollAreaHeight = this.endElement.offsetTop;
 		} else {
 			// setting the scroll area height to the full page height
 			scrollAreaHeight = Math.max(
@@ -193,9 +193,9 @@ export default class ReadingPosition {
 	 */
 	get min() {
 		// check wether an startElement is present and a valid DOM Node
-		if (this.$startElement && this.$startElement instanceof HTMLElement) {
+		if (this.startElement && this.startElement instanceof HTMLElement) {
 			// returning the top ofset of the element, making sure the value can't be below zero
-			return Math.max(0, this.$startElement.offsetTop);
+			return Math.max(0, this.startElement.offsetTop);
 		}
 
 		return 0;
